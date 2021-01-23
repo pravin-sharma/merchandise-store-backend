38ce9f71e3fcf87877fe98862666287d5ffacc00
PRAVIN SHARMA <pravinsharma9610@gmail.com>
2021-01-23 11:10:37 +0530

    Changed response payload for errors in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt');
 
@@ -7,61 +7,72 @@ exports.signup = (req, res, next) => {
 
     const errors = validationResult(req);
     console.log(errors)
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({
-            message: errors.errors[0].msg,
+            error: errors.errors[0].msg,
             param: errors.errors[0].param
         })
     }
 
     const user = new User(req.body);
     user.save()
-    .then(user =>{
-        res.status(200).json({
-            name: user.name,
-            email: user.email
+        .then(user => {
+            res.status(200).json({
+                name: user.name,
+                email: user.email
+            })
         })
-    })
-    .catch(err=>res.status(400).send(err))
+        .catch(err => {
+            console.log(err.code);
+            if (err.code == '11000') {
+                return res.status(400).json({
+                    error: "Account with this email already exist"
+                })
+            }
+
+            return res.status(400).json({
+                error: err.message
+            })
+        })
 }
 
-exports.signin = (req,res,next) =>{
-    const {email,password} = req.body;
+exports.signin = (req, res, next) => {
+    const { email, password } = req.body;
 
     //backend field validation
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({
-            message: errors.errors[0].msg,
+            error: errors.errors[0].msg,
             param: errors.errors[0].param
         })
     }
 
     //find user by email
-    User.findOne({email: email},(err, user)=>{
-         //if no email found or err in query or connection
-         if(err){
+    User.findOne({ email: email }, (err, user) => {
+        //if no email found or err in query or connection
+        if (err) {
             return res.status(400).json({
-                message: err
+                error: err
             })
         }
 
-        if(!user){
+        if (!user) {
             return res.status(400).json({
-                message: "User not Found"
+                error: "User not Found"
             })
         }
 
         //if email found, match the password
-        if(user.authenticate(password)){
-            const { _id, name, lastname,  email, role, purchases } = user
+        if (user.authenticate(password)) {
+            const { _id, name, lastname, email, role, purchases } = user
 
             //Creating token
-            const token = jwt.sign({'_id':_id}, process.env.SECRET);
-    
+            const token = jwt.sign({ '_id': _id }, process.env.SECRET);
+
             //put token in cookie inside res headers
-            res.cookie('token',token,{expire: new Date()+9999});
-    
+            res.cookie('token', token, { expire: new Date() + 9999 });
+
             //send response to front end
             return res.json({
                 token: token,
@@ -74,9 +85,9 @@ exports.signin = (req,res,next) =>{
                     purchases: purchases
                 }
             })
-        }else{
+        } else {
             return res.status(401).json({
-                message: "User Email or password incorrect"
+                error: "User Email or password incorrect"
             })
         }
     })
@@ -96,9 +107,9 @@ exports.isSignedIn = expressJwt({
 
 // custom middleware
 //isAuthenticated
-exports.isAuthenticated = (req,res,next) =>{
+exports.isAuthenticated = (req, res, next) => {
     const checker = req.profile && req.auth && req.profile._id == req.auth._id
-    if(!checker){
+    if (!checker) {
         return res.status(403).json({
             error: "ACCESS DENIED"
         })
@@ -107,8 +118,8 @@ exports.isAuthenticated = (req,res,next) =>{
 }
 
 //isAdmin
-exports.isAdmin = (req,res,next) =>{
-    if(req.profile.role == 0){
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role == 0) {
         return res.status(403).json({
             error: "You are not ADMIN, ACCESS DENIED"
         })
